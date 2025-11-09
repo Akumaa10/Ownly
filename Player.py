091b9cb6de44f51d93500e61789db7349f8b6417
RKY-L <143527631+RKY-L@users.noreply.github.com>
2025-11-09 05:47:54 -0500

    buying boots affect player boost and price of boosts aswell as the progress of boost stage

--- Player.py
+++ Player.py
@@ -1,14 +1,22 @@
-property_multiplier = {"pink": 1.1, "orange":1.2, "red":1.3, "yellow":1.4, "green":1.5, "dark_blue":1.6, "brown":1.7, "light_blue":1.8}
-
+property_value = {"pink":{0:250,1:300,2:360,3:576,4:691},
+                  "orange":{0:1250,1:1350,2:1500,3:1800,4:2160},
+                  "red":{0:2500,1:3000,2:3600,3:4320,4:5180},
+                  "yellow":{0:3750,1:4500,2:5400,3:6480,4:7780},
+                  "green":{0:12500,1:15000,2:18000,3:21600,4:25900},
+                  "blue":{0:25000,1:30000,2:36000,3:43200,4:51800},
+                  "brown":{0:125000,1:150000,2:180000,3:216000,4:259000},
+                  "cyan":{0:250000,1:300000,2:360000,3:432000,4:5180000}}
+boosts_multipler = {"roll":{1:10,2:30,3:90,4:270,5:810},"go":{1:1.1,2:1.25,3:1.5,4:1.75,5:2}}
 
 class Player:
     def __init__(self):
-        self.money = 2000
-        self.base_roll = 10
+        self.money = 100000000
+        self.base_roll = boosts_multipler["roll"][1]
         self.base_go = 200
         self.properties = {} #{'color_number':owned_state,...}  color_number = pink_1, owned_state = 0 (owned), 1 (1 classroom), etc up to 4 classrooms
         self.buffs = [] #double dice, prop_disc, 
         self.position = 32
+        self.boosts = {"roll":1,"go":1} 
         pass
 
     def update_property(self,property:str): #property = str "pink_1"
@@ -18,14 +26,14 @@ class Player:
             self.properties[property] = 0
     
     def pass_go(self): #user has passed go
-        total_multiplier = 0
+        prop_total = 0
         for color in self.properties:
-            total *= property_multiplier[color.split('_')[0]]
+            prop_total += property_value[color.split("_")[0]][self.properties[color]]
         
-        self.money += self.base_go * total_multiplier
+        self.money += (self.base_go + prop_total) * boosts_multipler["go"][self.boosts["go"]]
 
     def roll(self,dice_total:int): #user has rolled
-        self.money += self.base_roll * dice_total
+        self.money += boosts_multipler["roll"][self.boosts["roll"]] * dice_total
 
     def upgrade(self,upg_type:str): #upg_type = "roll"/"go"
         if(upg_type == "roll"):
@@ -38,3 +46,9 @@ class Player:
             self.buffs.append(buff)
             self.money -= price
 
+    def update_boosts(self,boost:str,price:int): #boost = "roll"/"go"
+        print(self.boosts[boost] < 5 and self.money >= price)
+        if self.boosts[boost] < 5 and self.money >= price:
+            self.boosts[boost] += 1
+            self.money -= price
+        
--- boosts.py
+++ boosts.py
@@ -2,29 +2,78 @@ import pygame
 import gc
 import pygame_gui
 
+import Player
+from helper import display_money,display_text
+
 def clear_screen(screen):
     screen.fill((255, 255, 255))
     pygame.display.flip()
     gc.collect()
 
-main_background = pygame.image.load("./images/boosts/main_page.png")
-right_button = pygame.image.load('./images/right_arrow.png')
-left_button = pygame.image.load('./images/left_arrow.png')
-lb_rect = pygame.Rect((34,519),left_button.get_size())
-rb_rect = pygame.Rect((259,519),right_button.get_size())
+imgs = {
+    "main_page":pygame.image.load("./images/boosts/main_page.png"),
+    "right_button":pygame.image.load('./images/right_arrow.png'),
+    "left_button":pygame.image.load('./images/left_arrow.png'),
+    "buy_roll":pygame.image.load('./images/boosts/buy_roll.png'),
+    "buy_go":pygame.image.load('./images/boosts/buy_go.png'),
+    "1_bar_1":pygame.image.load('./images/boosts/1_bar_1.png'),
+    "2_bar_2":pygame.image.load('./images/boosts/2_bar_2.png'),
+    "3_bar_3":pygame.image.load('./images/boosts/3_bar_3.png'),
+    "4_bar_4":pygame.image.load('./images/boosts/4_bar_4.png'),
+    "5_bar_5":pygame.image.load('./images/boosts/5_bar_5.png'),
+    "1_bar":pygame.image.load('./images/boosts/1_bar.png'),
+    "2_bar":pygame.image.load('./images/boosts/2_bar.png'),
+    "3_bar":pygame.image.load('./images/boosts/3_bar.png'),
+    "4_bar":pygame.image.load('./images/boosts/4_bar.png'),
+    "5_bar":pygame.image.load('./images/boosts/5_bar.png')
+}
+
+rect = {
+    "right_button":pygame.Rect((259,519),imgs["right_button"].get_size()),
+    "left_button":pygame.Rect((34,519),imgs["left_button"].get_size()),
+    "buy_roll":pygame.Rect((101,252),imgs["buy_roll"].get_size()),
+    "buy_go":pygame.Rect((101,392),imgs["buy_go"].get_size()),
+    "1_bar_1":pygame.Rect((101,375),imgs["1_bar_1"].get_size()),
+    "2_bar_2":pygame.Rect((101,375),imgs["2_bar_2"].get_size()),
+    "3_bar_3":pygame.Rect((101,375),imgs["3_bar_3"].get_size()),
+    "4_bar_4":pygame.Rect((101,375),imgs["4_bar_4"].get_size()),
+    "5_bar_5":pygame.Rect((101,375),imgs["5_bar_5"].get_size()),
+    "1_bar":pygame.Rect((101,235),imgs["1_bar"].get_size()),
+    "2_bar":pygame.Rect((101,235),imgs["2_bar"].get_size()),
+    "3_bar":pygame.Rect((101,235),imgs["3_bar"].get_size()),
+    "4_bar":pygame.Rect((101,235),imgs["4_bar"].get_size()),
+    "5_bar":pygame.Rect((101,235),imgs["5_bar"].get_size())
+}
+
+boost_price = {1:300,2:600,3:1200,4:2400,5:"Sold Out"}
 
 def render_boosts(screen,player):
-    screen.blit(main_background, (0,0))
-    screen.blit(right_button, rb_rect)
-    screen.blit(left_button, lb_rect)
-
-
     def button_clicked(screen,event_pos):
-        if(lb_rect.collidepoint(event_pos)):
+        if(rect["left_button"].collidepoint(event_pos)):
             return "buffs"
-        if(rb_rect.collidepoint(event_pos)):
+        elif(rect["right_button"].collidepoint(event_pos)):
             return "leaderboard"
+        elif(rect['buy_roll'].collidepoint(event_pos)):
+            player.update_boosts("roll",boost_price[player.boosts["roll"]])
+        elif(rect['buy_go'].collidepoint(event_pos)):
+            player.update_boosts("go",boost_price[player.boosts["go"]])
+        
+        render_boosts(screen,player)
         return None
+    
+    for img in imgs:
+        if(img[0].isalpha() or 
+           (img[0].isdigit() and img[-1].isdigit() and int(img[0]) == player.boosts["go"]) or
+           (img[0].isdigit() and not img[-1].isdigit() and int(img[0]) == player.boosts["roll"])): #means it is roll
+            screen.blit(imgs[img], rect.get(img,(0,0)))
+
+
+    roll_text = "$"+str(boost_price[player.boosts["roll"]]) if player.boosts["roll"] != 5 else str(boost_price[player.boosts["roll"]])
+    go_text = "$"+str(boost_price[player.boosts["go"]]) if player.boosts["go"] != 5 else str(boost_price[player.boosts["go"]])
+
+    display_text(screen,roll_text,25,(255,255,255),"pixelifysansregular",(173,265))
+    display_text(screen,go_text,25,(255,255,255),"pixelifysansregular",(173,405))
+    display_money(screen,player)
 
     return button_clicked
 
